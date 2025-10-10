9eb96cff908f85c9506729afa0487a9fc390603d
Bmoneydawg <Baron.H300@gmail.com>
2025-10-10 19:34:12 -0400

    Completed array_fill

--- array_fill.c
+++ array_fill.c
@@ -4,5 +4,25 @@
  * TODO: Describe what the function does
  */
 int array_fill(int* array, int array_len, int begin, int end) {
-    return 1;
+    /* Validate inputs */
+    if (array == NULL) {
+        return 0;
+    }
+    if (array_len <= 0) {
+        return 0;
+    }
+    if (begin > end) {
+        /* Do not modify array when range is invalid */
+        return 0;
+    }
+
+    int count = 0;
+    int value = begin;
+
+    /* Fill the array with values from begin to end */
+    for (int i = 0; i < array_len && value <= end; ++i, ++value) {
+        array[i] = value;
+        ++count;
+    }
+    return count;
 }
